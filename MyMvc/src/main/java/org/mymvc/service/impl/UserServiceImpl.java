02ce9c526885f7881d217c87e3f5ec1569e396be
shiming-miku <shimingjim@163.com>
2016-11-03 18:59:39 +0800

    service 注解事务配置

--- MyMvc/src/main/java/org/mymvc/service/impl/UserServiceImpl.java
+++ MyMvc/src/main/java/org/mymvc/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -28,6 +29,7 @@ public class UserServiceImpl implements UserService {
 		
 	}
 
+	@Transactional("transactionManager")
 	@Override
 	public User selectByPrimaryKey(Long userId) {
 		logger.info("UserServiceImpl userId:"+userId+"查询");
